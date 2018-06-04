8bba3bdc7a31be44b1f48b8afd032978a84c408f
Thabo Thage <tebohothg@gmail.com>
2018-06-04 22:43:12 +0200

    Added animation controller files

---
+++ animation.h
@@ -0,0 +1,12 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+#include <map>
+
+class Animation {
+public:
+	Animation::Animation(int numOfSpawnPositions);
+	std::map<int,float> endTimes;
+	void setEndTime(int index, float currentTime);
+	bool checkExpiration(int position, float currentTime);
+};
+#endif
