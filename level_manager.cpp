232233737270924e58ed99f6168bee8420bd49dc
Thabo Thage <tebohothg@gmail.com>
2018-06-13 00:19:26 +0200

    Added shape spawning feature

--- level_manager.cpp
+++ level_manager.cpp
@@ -1,20 +1,17 @@
-#include "level_manager.h"
-#include "utilities.h"
-#include "resource_manager.h"
 #include <iostream>
 #include <random>
 #include <chrono>
 #include <algorithm>
 #include <sstream>
+#include "level_manager.h"
+#include "utilities.h"
+#include "resource_manager.h"
 
 LevelManager::LevelManager(const char* fontName):font{ fontName }, animController{9}, numCurrentShape{0}, totalShapes{0}
 {
 	currentState = StateMachine::START;
-	activeShapes.reserve(5);
 	createShapes();
 	initSpawnPositions();
-	animTimers.reserve(5);
-	counters.reserve(5);
 }
 
 void LevelManager::gameLoop()
@@ -22,13 +19,11 @@ void LevelManager::gameLoop()
 	switch (currentState)
 	{
 	case StateMachine::START:
-		firstSpawnShapes();
-		secondSpawnShapes();
-		setModelMatrices();
 		currentState = StateMachine::PLAY;
 		break;
 	case StateMachine::PLAY:
-	//	displayShapeText(selectedShape);
+		randomSpawnShapes();
+		//displayShapeText();
 		break;
 	default:
 		break;
@@ -55,7 +50,6 @@ void LevelManager::createShapes()
 			modelShapes[index].shapeType = static_cast<Shapes3D>(i);
 			modelShapes[index].loadModel(shapes[i]);
 			++index;
-		//	std::cout << "index = " << index << std::endl;
 		}
 	}
 }
@@ -64,28 +58,28 @@ void LevelManager::initSpawnPositions()
 {
 	//Front Row
 	PositionInfo p0 = { glm::vec3(-100.0f, 0.0f, -100.0f), nullptr };
-	spawnPostions.insert(std::make_pair(0, p0));
+	spawnPositions.insert(std::make_pair(0, p0));
 	PositionInfo p1 = { glm::vec3(0.0f, 0.0f, -100.0f), nullptr };
-	spawnPostions.insert(std::make_pair(1, p1));
+	spawnPositions.insert(std::make_pair(1, p1));
 	PositionInfo p2 = { glm::vec3(100.0f, 0.0f, -100.0f), nullptr };
-	spawnPostions.insert(std::make_pair(2, p2));
+	spawnPositions.insert(std::make_pair(2, p2));
 	//Middel Row
 	PositionInfo p3 = { glm::vec3(-100.0f, 0.0f, 0.0f), nullptr };
-	spawnPostions.insert(std::make_pair(3, p3));
+	spawnPositions.insert(std::make_pair(3, p3));
 	PositionInfo p4 = { glm::vec3(0.0f, 0.0f, 0.0f), nullptr };
-	spawnPostions.insert(std::make_pair(4, p4));
+	spawnPositions.insert(std::make_pair(4, p4));
 	PositionInfo p5 = { glm::vec3(100.0f, 0.0f, 0.0f), nullptr };
-	spawnPostions.insert(std::make_pair(5, p5));
+	spawnPositions.insert(std::make_pair(5, p5));
 	//Back Row
 	PositionInfo p6 = { glm::vec3(-100.0f, 0.0f, 100.0f), nullptr };
-	spawnPostions.insert(std::make_pair(6, p6));
+	spawnPositions.insert(std::make_pair(6, p6));
 	PositionInfo p7 = { glm::vec3(0.0f, 0.0f, 100.0f), nullptr };
-	spawnPostions.insert(std::make_pair(7, p7));
+	spawnPositions.insert(std::make_pair(7, p7));
 	PositionInfo p8 = { glm::vec3(100.0f, 0.0f, 100.0f), nullptr };
-	spawnPostions.insert(std::make_pair(8, p8));
+	spawnPositions.insert(std::make_pair(8, p8));
 }
 
-int LevelManager::randomSelection(int min, int max)
+int LevelManager::randomInt(int min, int max)
 {
 	// random number engine
 	static std::default_random_engine e;
@@ -96,210 +90,220 @@ int LevelManager::randomSelection(int min, int max)
 	return distr(e);
 }
 
-GLuint LevelManager::findSpawnPositions()
+float LevelManager::randomReal(float min, float max)
+{
+	// random number engine
+	static std::default_random_engine e;
+	//random number engine seed
+	unsigned int seedValue = std::chrono::steady_clock::now().time_since_epoch().count();
+	e.seed(seedValue);
+	std::uniform_real_distribution<float> distr(min, max);
+	return distr(e);
+}
+
+GLuint LevelManager::findSpawnPositions() noexcept
 {
 	GLuint positionIndex;
 	while (true) {
-		positionIndex = randomSelection(0, 8);
-		if (spawnPostions[positionIndex].activeObject == nullptr) {
-			printf("POS = %d ", positionIndex);
+		positionIndex = randomInt(0, 8);
+		if (spawnPositions[positionIndex].activeObject == nullptr) {
 			break;
 		}
 	}
 	return positionIndex;
 }
 
+void LevelManager::initPositionInfo(GLuint posIndex)
+{
+	activeShapes.emplace_back(ActiveShapeProperties{ 0, 0, Clock(), Clock(), glm::mat4() });
+	auto p = activeShapes.rbegin();
+	setModelMatrix(posIndex, p);
+	p->posIndex = posIndex;
+	p->globalTimer.startClock();
+	p->animTimer.startClock();
+	p->animTimer.addTime();
+	float offset = randomReal(2.0, 5.0f);
+	float lifeSpan = p->animTimer.getElapsedTime() + offset;
+	animController.setEndTime(posIndex, lifeSpan);
+}
+
 void LevelManager::firstSpawnShapes()
 {
-	selectedShape = randomSelection(0, 9);
+	selectedShape = randomInt(0, 9);
 	GLuint positionIndex;
 	constexpr unsigned int STRIDE = 3;
+
 	//First place 3 shapes of the selected type on the level
-	//Shape 1
-	positionIndex = findSpawnPositions();
-	int index = selectedShape * STRIDE;
-	spawnPostions[positionIndex].activeObject = (modelShapes + index);
-	activeShapes.push_back(positionIndex);
-	printf("shape = %d , pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-	animTimers.emplace_back(Clock());
-	animTimers[0].startClock();
-	globalTimer[0].startClock();
-	animTimers[0].addTime();
-	animController.setEndTime(positionIndex, animTimers[0].getElapsedTime());
-	counters.push_back(0);
-	////Shape 2
-	//positionIndex = findSpawnPositions();
-	//index = selectedShape * STRIDE + 1;
-	//spawnPostions[positionIndex].activeObject = (modelShapes + index);
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-	////Shape 3
-	//positionIndex = findSpawnPositions();
-	//index = selectedShape * STRIDE + 2;
-	//spawnPostions[positionIndex].activeObject = (modelShapes + index);
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-}
-
-void LevelManager::secondSpawnShapes()
-{
-	//Place 3 other shapes on the grid
-	//Shape 4
-	//int randomSelectedShape = randomSelection(0, 29);
-	//GLuint positionIndex = findSpawnPositions();
-	//spawnPostions[positionIndex].activeObject = &modelShapes[randomSelectedShape];
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-	////Shape 5
-	//randomSelectedShape = randomSelection(0, 29);
-	//positionIndex = findSpawnPositions();
-	//spawnPostions[positionIndex].activeObject = &modelShapes[randomSelectedShape];
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-	//Shape 6
-	//randomSelectedShape = randomSelection(0, 29);
-	//positionIndex = findSpawnPositions();
-	//spawnPostions[positionIndex].activeObject = &modelShapes[randomSelectedShape];
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-	////Shape 7
-	//randomSelectedShape = randomSelection(0, 29);
-	//positionIndex = findSpawnPositions();
-	//spawnPostions[positionIndex].activeObject = &modelShapes[randomSelectedShape];
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-	////Shape 8
-	//randomSelectedShape = randomSelection(0, 29);
-	//positionIndex = findSpawnPositions();
-	//spawnPostions[positionIndex].activeObject = &modelShapes[randomSelectedShape];
-	//activeShapes.push_back(positionIndex);
-	//printf("shape = %d ,  pos = %d\n", spawnPostions[positionIndex].activeObject->shapeType, positionIndex);
-}
-
-void LevelManager::setModelMatrices()
-{
-	PositionInfo temp;
-	for (unsigned int i = 0; i < activeShapes.size(); ++i) {
-		temp = spawnPostions[activeShapes[i]];
-		if (temp.activeObject != nullptr) {
-			setModelMatrix(temp);
-		}
+	for (int i = 0; i < 3; ++i) {
+		positionIndex = findSpawnPositions();
+		int index = selectedShape * STRIDE + i;
+		spawnPositions[positionIndex].activeObject = (modelShapes + index);
+		initPositionInfo(positionIndex);
+		
+	}
+	
+	//Place 3 other shapes on the level
+	for (int i = 1; i <= 3; ++i) {
+		int randomSelectedShape = randomInt(0, 29);
+		positionIndex = findSpawnPositions();
+		spawnPositions[positionIndex].activeObject = &modelShapes[randomSelectedShape];
+		initPositionInfo(positionIndex);
 	}
 }
 
-void LevelManager::setModelMatrix(PositionInfo& spawnPosition)
+void LevelManager::randomSpawnShapes()
 {
-	Model* shape = spawnPosition.activeObject;
-	Shapes3D type = shape->shapeType;
+	std::size_t listSize = activeShapes.size();
+	if (listSize > 0 && listSize < 6){
+		GLuint pos;
+		constexpr int STRIDE = 3;
+		int randomSelectedShape;
+		auto num = std::count_if(std::begin(activeShapes), std::end(activeShapes), 
+			[&position = spawnPositions, &shapeType = selectedShape](ActiveShapeProperties& shape) 
+			{ return position[shape.posIndex].activeObject->shapeType == static_cast<Shapes3D>(shapeType); });
+	    if (num == 0) {
+			for (int i = 0; i < 2; ++i) {
+				pos = findSpawnPositions();
+				int index = selectedShape * STRIDE + i;
+				spawnPositions[pos].activeObject = (modelShapes + index);
+				initPositionInfo(pos);
+			}
+		}
+		else if (num == 3){
+			do {
+				randomSelectedShape = randomInt(0, 29);
+			} while (randomSelectedShape == selectedShape);
+			pos = findSpawnPositions();
+			spawnPositions[pos].activeObject = &modelShapes[randomSelectedShape];
+			initPositionInfo(pos);
+		}
+		else if ((num >= 0 && num < 3) || num > 3) {
+			randomSelectedShape = randomInt(0, 29);
+			pos = findSpawnPositions();
+			spawnPositions[pos].activeObject = &modelShapes[randomSelectedShape];
+			initPositionInfo(pos);
+		}//if-else
+	}
+	else if(listSize == 0) {
+		firstSpawnShapes();
+	}//if-else
+}//randomSpawnShapes
+
+void LevelManager::setModelMatrix(GLuint position, std::list<ActiveShapeProperties>:: reverse_iterator iter)
+{
+	Shapes3D type = spawnPositions[position].activeObject->shapeType;
 	glm::mat4 modelMatrix;
 
 	//Place shape in correct world position
 	switch (type)
 	{
-	case Shapes3D::R_PRISM:
-		spawnPosition.worldPosition.y = 1.8f;
-		modelMatrix = glm::translate(modelMatrix, spawnPosition.worldPosition);
-		modelMatrix = glm::scale(modelMatrix, glm::vec3(3.0f, 3.0f, 3.0f));
-		shapeModelMatrices.push_back(modelMatrix);
-		break;
-	case Shapes3D::T_PRISM:
-		spawnPosition.worldPosition.y = 1.0f;
-		modelMatrix = glm::translate(modelMatrix, spawnPosition.worldPosition);
-		modelMatrix = glm::scale(modelMatrix, glm::vec3(3.0f, 3.0f, 3.0f));
-		shapeModelMatrices.push_back(modelMatrix);
-		break;
-	case Shapes3D::R_PYRAMID:
-		spawnPosition.worldPosition.y = 2.5f;
-		modelMatrix = glm::translate(modelMatrix, spawnPosition.worldPosition);
-		modelMatrix = glm::scale(modelMatrix, glm::vec3(5.0f, 5.0f, 5.0f));
-		shapeModelMatrices.push_back(modelMatrix);
-		break;
+	case Shapes3D::R_PRISM:	spawnPositions[position].worldPosition.y = 1.8f;
+			modelMatrix = glm::translate(modelMatrix, spawnPositions[position].worldPosition);
+			modelMatrix = glm::scale(modelMatrix, glm::vec3(3.0f, 3.0f, 3.0f));
+			break;
+	case Shapes3D::T_PRISM: spawnPositions[position].worldPosition.y = 1.0f;
+			modelMatrix = glm::translate(modelMatrix, spawnPositions[position].worldPosition);
+			modelMatrix = glm::scale(modelMatrix, glm::vec3(3.0f, 3.0f, 3.0f));
+			break;
+	case Shapes3D::R_PYRAMID:spawnPositions[position].worldPosition.y = 2.5f;
+			modelMatrix = glm::translate(modelMatrix, spawnPositions[position].worldPosition);
+			modelMatrix = glm::scale(modelMatrix, glm::vec3(5.0f, 5.0f, 5.0f));
+			break;
 	case Shapes3D::ELLIPSOID:
-	case Shapes3D::T_PYRAMID:
-		spawnPosition.worldPosition.y = 1.0f;
-		modelMatrix = glm::translate(modelMatrix, spawnPosition.worldPosition);
-		modelMatrix = glm::scale(modelMatrix, glm::vec3(5.0f, 5.0f, 5.0f));
-		shapeModelMatrices.push_back(modelMatrix);
-		break;
-	default:
-		spawnPosition.worldPosition.y = 0.0f;
-		modelMatrix = glm::translate(modelMatrix, spawnPosition.worldPosition);
-		modelMatrix = glm::scale(modelMatrix, glm::vec3(5.0f, 5.0f, 5.0f));
-		shapeModelMatrices.push_back(modelMatrix);
+	case Shapes3D::T_PYRAMID: spawnPositions[position].worldPosition.y = 1.0f;
+			modelMatrix = glm::translate(modelMatrix, spawnPositions[position].worldPosition);
+			modelMatrix = glm::scale(modelMatrix, glm::vec3(5.0f, 5.0f, 5.0f));
+			break;
+	default:spawnPositions[position].worldPosition.y = 0.0f;
+			modelMatrix = glm::translate(modelMatrix, spawnPositions[position].worldPosition);
+			modelMatrix = glm::scale(modelMatrix, glm::vec3(5.0f, 5.0f, 5.0f));
 	}
-}
+	iter->shapeModelMatrix = modelMatrix;
+}//setModelMatrix
 
 void LevelManager::renderShadows(GLuint programID, float deltaTime)
 {
-	for (unsigned int i = 0; i < activeShapes.size(); ++i) {
-		int index = activeShapes[i];
-		PositionInfo temp = spawnPostions[index];
-		if (counters[i] <= STOP_COUNT) {
-			globalTimer[i].addTime();
-			animTimers[i].addTime();
-			if (animTimers[i].getElapsedTime() < animController.endTimes[index]) {
-				shapeModelMatrices[i] = glm::rotate(shapeModelMatrices[i], 1.0f * deltaTime, glm::vec3(1.0f, 1.0f, 1.0f));
-				glUniformMatrix4fv(glGetUniformLocation(programID, "model"), 1, GL_FALSE, glm::value_ptr(shapeModelMatrices[i]));
+	auto iter = activeShapes.begin();
+	while (iter != activeShapes.end()) {
+		int index = iter->posIndex;
+		PositionInfo temp = spawnPositions[index];
+		int blinkCounter = iter->counter;
+		if (blinkCounter >= 0 && blinkCounter <= STOP_COUNT) {
+			iter->globalTimer.addTime();
+			iter->animTimer.addTime();
+			if (iter->animTimer.getElapsedTime() < animController.endTimes[index]) {
+				iter->shapeModelMatrix = glm::rotate(iter->shapeModelMatrix, 1.0f * deltaTime, glm::vec3(1.0f, 1.0f, 1.0f));
+				glUniformMatrix4fv(glGetUniformLocation(programID, "model"), 1, GL_FALSE, glm::value_ptr(iter->shapeModelMatrix));
 				temp.activeObject->Draw();
 			}
 			else {
-				if (globalTimer[i].getElapsedTime() > 1.5f) {
+				if (iter->globalTimer.getElapsedTime() > 1.4f) {
 					animController.endTimes[index] = 1.0f;
-					animTimers[i].stopClock();
-					animTimers[i].startClock();
-					globalTimer[i].stopClock();
-					globalTimer[i].startClock();
-					++counters[i];
+					iter->animTimer.stopClock();
+					iter->animTimer.startClock();
+					iter->globalTimer.stopClock();
+					iter->globalTimer.startClock();
+					iter->counter += 1;
 				}//if
 			}//if-else
 		}//if
+		if (blinkCounter < 0 || blinkCounter > STOP_COUNT) {
+			spawnPositions[iter->posIndex].activeObject = nullptr;
+			iter = activeShapes.erase(iter);
+		}
 		else {
-			temp.activeObject = nullptr;
+			++iter;
 		}
 	}//for loop
-}
+}//:renderShadows
 
 void LevelManager::drawShapes(std::shared_ptr<Shader>shaderObject, glm::vec3& cameraPos)
 {
-	for (unsigned int i = 0; i < activeShapes.size(); ++i) {
-		if (counters[i] <= STOP_COUNT) {
-			globalTimer[i].addTime();
-			animTimers[i].addTime();
-			shaderObject->setMatrix(shapeModelMatrices[i], "model");
-			int index = activeShapes[i];
-			float passedTime = animTimers[i].getElapsedTime();
+	for (auto iter = activeShapes.begin(); iter != activeShapes.end(); ++iter) {
+		int counter = iter->counter;
+		if (counter >= 0 && counter <= STOP_COUNT)
+		{
+			iter->globalTimer.addTime();
+			iter->animTimer.addTime();
+			shaderObject->setMatrix(iter->shapeModelMatrix, "model");
+			int index = iter->posIndex;
+			float passedTime = iter->animTimer.getElapsedTime();
 			if (passedTime < animController.endTimes[index]) {
-				PositionInfo p = spawnPostions[index];
+				PositionInfo p = spawnPositions[index];
 				p.activeObject->setLightUniforms(shaderObject, cameraPos);
 				p.activeObject->Draw();
 			}//if
 		}//if
 	}//for loop
-}
+}//drawShapes
 
 void LevelManager::displayShapeText()
 {
 	std::string shape{""};
 	switch (selectedShape)
 	{
-	case 0:shape = "CONES";break;
-	case 1:shape = "CUBES";break;
-	case 2:shape = "CYLINDERS";break;
-	case 3:shape = "ELLIPSOIDS";break;
+	case 0:shape = "CONE";break;
+	case 1:shape = "CUBE";break;
+	case 2:shape = "CYLINDER";break;
+	case 3:shape = "ELLIPSOID";break;
 	case 4:shape = "HEMISPHERE";break;
-	case 5:shape = "R_PRISM"; break;
-	case 6:shape = "R_PYRAMID"; break;
+	case 5:shape = "RECTANGULAR PRISM"; break;
+	case 6:shape = "RECTANGULAR PYRAMID"; break;
 	case 7:shape = "SPHERE"; break;
-	case 8:shape = "T_PRISM"; break;
-	case 9:shape = "T_PYRAMID"; break;
+	case 8:shape = "TRIANGULAR PRISM"; break;
+	case 9:shape = "TRIANGULAR PYRAMID"; break;
 	}
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	std::ostringstream resultsDisplayText;
+	std::ostringstream resultsDisplayText, healthDisplayText;
 	resultsDisplayText << shape << " X " << numCurrentShape;
+	//health -= 1.0f;
+	healthDisplayText << "HEALTH " << health;
+	std::string s_health{ healthDisplayText.str() };
 	std::string temp{ resultsDisplayText.str() };
-	font.renderCharacters(ResourceManager::GetShader("fontShader"), temp, 250.0f, 300.0f, 1.0f, glm::vec3(1.0f, 0.84f, 0.0f));
+	font.renderCharacters(ResourceManager::GetShader("fontShader"), s_health, 5.0f, 540.0f, 0.3f, glm::vec3(1.0f, 0.84f, 0.0f));
+	font.renderCharacters(ResourceManager::GetShader("fontShader"), temp, 5.0f, 520.0f, 0.3f, glm::vec3(1.0f, 0.84f, 0.0f));
 	glDisable(GL_BLEND);
-}
+}//displayShapeText
 
 
--- level_manager.h
+++ level_manager.h
@@ -16,39 +16,45 @@ struct PositionInfo {
 	Model* activeObject;
 };
 
+struct ActiveShapeProperties {
+	GLuint posIndex;
+	int counter;
+	Clock globalTimer;
+	Clock animTimer;
+	glm::mat4 shapeModelMatrix;
+};
+
 enum class StateMachine { START, PLAY, GAME_OVER };
 
 class LevelManager {
 public:
-	LevelManager(const char* fontName);
-	std::map<GLuint, PositionInfo> spawnPostions;
-	std::vector<GLuint>activeShapes;
+	std::map<GLuint, PositionInfo> spawnPositions;
+	std::list<ActiveShapeProperties>activeShapes;
+	int selectedShape;
 	Model* modelShapes;
 	Animation animController;
 	StateMachine currentState;
 	Font font;
-	std::vector<glm::mat4>  shapeModelMatrices;
+	LevelManager(const char* fontName);
 	void renderShadows(GLuint programID, float deltaTime);
 	void drawShapes(std::shared_ptr<Shader> shaderObj, glm::vec3 & cameraPos);
 	void displayShapeText();
 	void gameLoop();
 private:
 	const GLuint NUM_SHAPES = 30;
-	int selectedShape;
 	int numCurrentShape;
 	int totalShapes;
-	std::vector<Clock> animTimers;
-	std::vector<int> counters;
 	const int STOP_COUNT = 6;
-	Clock globalTimer[5];
+	float health = 100.0f;
 	void createShapes();
 	void initSpawnPositions();
 	void firstSpawnShapes();
-	void secondSpawnShapes();
-	void setModelMatrices();
-	void setModelMatrix(PositionInfo & spawnPosition);
-	int randomSelection(int min, int max);
-	GLuint findSpawnPositions();
+	void randomSpawnShapes();
+	void setModelMatrix(GLuint position, std::list<ActiveShapeProperties>::reverse_iterator iter);
+	int randomInt(int min, int max);
+	float randomReal(float min, float max);
+	GLuint findSpawnPositions() noexcept;
+	void initPositionInfo(GLuint posIndex);
 };
 
 #endif
