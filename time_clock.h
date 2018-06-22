34552d16f28acd5f57814f18009fec202c707e22
Thabo Thage <tebohothg@gmail.com>
2018-06-22 14:32:24 +0200

    Made timer variable public

--- time_clock.h
+++ time_clock.h
@@ -11,12 +11,11 @@ public:
 	void addTime();
 	void stopClock();
 	float getElapsedTime() const;
-
+	bool stopTime;
+	float timer;
 private:
 	std::chrono::time_point<std::chrono::steady_clock> start_ticks;
 	std::chrono::time_point<std::chrono::steady_clock> end_ticks;
 	std::chrono::duration<double> timeDuration;
-	float timer;
-	bool stopTime;
 };
 #endif
